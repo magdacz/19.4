37b398eb50a091e777ed3728489dc2831de983ab
Magda Czaicka-Chmielewska <czaicka.chmielewska@op.pl>
2018-07-10 19:39:01 +0200

    Add files

---
+++ actions.js
@@ -0,0 +1,58 @@
+
+import uuid from uuid;
+
+export const ADD_COMMENT = 'ADD_COMMENT';
+export const REMOVE_COMMENT = 'REMOVE_COMMENT';
+export const EDIT_COMMENT = 'EDIT_COMMENT';
+export const THUMB_UP_COMMENT = 'THUMB_UP_COMMENT';
+export const THUMB_DOWN_COMMENT = 'THUMB_DOWN_COMMENT';
+
+function addComment(text) {
+    return {
+        type: ADD_COMMENT,
+        text,
+        id: uuid.v4()
+    }
+}
+
+const boundAddComment = text => dispatch(addComment(text));
+
+function removeComment(id) {
+    return {
+        type: REMOVE_COMMENT,
+        id
+    }
+}
+
+const boundRemoveComment = id => dispatch(removeComment(id));
+
+function editComment(text, id) {
+    return {
+        type: EDIT_COMMENT,
+        text,
+        id
+    }
+}
+
+const EditComment = (text,id) => dispatch(editComment(text,id));
+
+function thumbUpComment(id) {
+    return {
+        type: THUMB_UP_COMMENT,
+        id
+    }
+}
+
+const boundThumbUpComment = id => dispatch(thumbUpComment(id));
+
+function thumbDownComment(id) {
+    return {
+        type: THUMB_DOWN_COMMENT,
+        id
+    }
+}
+
+const boundThumbDownComment = id => dispatch(thumpDownComment(id));
+
+
+
